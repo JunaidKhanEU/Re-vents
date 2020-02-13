a63b3b8d2b186634ed769e5a230a59a592ad5c69
JunaidKhanEU <junaidkhan1@gmx.com>
2020-02-13 12:27:26 +0000

    scrollToTop Fix

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store'
+import ScrollToTop from './utils/ScrollToTop/ScrollToTop'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </Provider>
